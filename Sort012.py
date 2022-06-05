13ea1c221c238ed11558a74965e257a71fff3f35
Prateek Aryan <praryan@cisco.com>
2022-06-06 01:52:27 +0530

    Sort012

---
+++ Sort012.py
@@ -0,0 +1,53 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**7)
+
+def sort012(arr, n) :
+
+	# write your code here
+    # don't return anything 
+    a=[]
+    c=0
+    for i in range(len(arr)):
+        if arr[i]==0:
+            arr[c],arr[i]=arr[i],arr[c]
+            c+=1
+    for i in range(len(arr)):
+        if arr[i]==1:
+            arr[c],arr[i]=arr[i],arr[c]
+            c+=1
+
+
+#taking inpit using fast I/O
+def takeInput() :
+	n = int(input().strip())
+
+	if n == 0 :
+		return list(), 0
+
+	arr = list(map(int, stdin.readline().strip().split(" ")))
+
+	return arr, n
+
+
+
+def printAnswer(arr, n) :
+    
+    for i in range(n) :
+        
+        print(arr[i],end=" ")
+        
+    print()
+    
+#main
+
+t = int(input().strip())
+for i in range(t) :
+
+    arr, n= takeInput()
+    sort012(arr, n)
+    printAnswer(arr, n)
